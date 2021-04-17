11e4f1e990bbf4126fafb27ddd9feee6fa1c5bfe
severine legon <severine@multiply.ai>
2021-04-17 15:51:10 +0100

    chore: merge conflicts and setup of routing

--- src/containers/Routes/Routes.jsx
+++ src/containers/Routes/Routes.jsx
@@ -2,13 +2,21 @@ import { Router } from "@reach/router";
 import React, { Component } from "react";
 import ItemListings from "../ItemListings/ItemListings";
 import ItemView from "../ItemView/ItemView";
+import Home from "../../containers/Home";
+import Donate from "../DonatePage/Donate";
+import DonateComplete from "../DonateComplete/DonateComplete";
+import ReserveForm from "../ReserveFormPage/ReserveFormPage";
 
 class Routes extends Component {
   render() {
     return (
       <Router>
-        <ItemListings default />
+        <Home default />
+        <ItemListings path="/listings" />
         <ItemView path="/itemView/:name/:make/:price/:condition/" />
+        <Donate path="/donate" />
+        <DonateComplete path="/donatecomplete" />
+        <ReserveForm path="/reserveform" />
       </Router>
     );
   }
