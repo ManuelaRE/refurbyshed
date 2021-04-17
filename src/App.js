b0e6bf1602136b5d4b63d78a2392f6453b823368
Manuela Redinciuc <manuela.redinciuc@brickvest.com>
2021-04-17 14:22:56 +0100

    WhatWeDo page initial setup.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-
 import LandingPage from "./components/LandingPage";
+import WhatWeDo from "./components/WhatWeDo";
+
 
 function App() {
   return (
-    <LandingPage/>
+    <>
+      <LandingPage/>
+      <WhatWeDo/>
+    </>
   );
 }
 
