595e863707ce73284eadb4da1007d3d4ef661b00
Tasha Stenner <tashastenner@Tashas-Air.broadband>
2021-04-17 18:25:49 +0100

    added image into reserve complete

--- src/containers/ReserveComplete/ReserveComplete.jsx
+++ src/containers/ReserveComplete/ReserveComplete.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import styles from "./ReserveComplete.module.scss";
 import Image from 'react-bootstrap/Image';
-import complete from '../../assets/Icons/complete.png';
+import complete from '../../assets/Icons/complete.svg';
 
 const ReserveComplete = () => {
   return (
     <section className={styles.container}>
       <h2 className={styles.title} >Thank you for your reservation </h2>
-      <Image rounded src={logo} height="250"/>
+      <Image rounded src={complete} height="250"/>
       <p>You will recieve an email within the next 24 hours from one of our friendly team members regarding</p>
     </section>
   );
